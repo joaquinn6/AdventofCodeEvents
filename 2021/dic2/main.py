6d38c0b10cfe5e1224ccae63d6fcfcbdfb5e0605
Joaquin <joaquin.nunez@hotaka.io>
2021-12-03 08:09:59 -0600

    simplificando

--- 2021/dic2/main.py
+++ 2021/dic2/main.py
@@ -1,29 +1,14 @@
 import ordenes
-
-class reto2:
-
-  def read_line(self, indice) -> dict:
-    print(indice)
-    orden = ordenes.ordenes()[indice]
-    return orden
-
-  def main(self):
-    i=0
-    x=0
-    y=0
-    aim=0
-    orden = self.read_line(i)
-    while orden['direction']!='exit':
-      if (orden['direction']=='down'):
-        aim+=orden['num']
-      if (orden['direction']=='up'):
-        aim-=orden['num']
-      if (orden['direction']=='forward'):
-        x+=orden['num']
-        y+=orden['num']*aim
-      i+=1
-      orden = self.read_line(i)
-    print('result=>' + str(x*y))
-
-reto=reto2()
-reto.main()
+i,x,y,aim=0,0,0,0
+orden = ordenes.ordenes()[i]
+while orden['direction']!='exit':
+  if (orden['direction']=='down'):
+    aim+=orden['num']
+  if (orden['direction']=='up'):
+    aim-=orden['num']
+  if (orden['direction']=='forward'):
+    x+=orden['num']
+    y+=orden['num']*aim
+  i+=1
+  orden = ordenes.ordenes()[i]
+print('result=>' + str(x*y))
