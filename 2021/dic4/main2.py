9a00489e01be4eb9986d2fb64254b20ba7bb658d
Joaquin <joaquin.nunez@hotaka.io>
2021-12-04 08:56:22 -0600

    4.2

---
+++ 2021/dic4/main2.py
@@ -0,0 +1,47 @@
+import bingo
+tableros, numeros, marcados, tabsGanadores = bingo.tableros(), bingo.numeros(), [], []
+
+def tableroCheck(iTablero, iRow, index) -> bool:
+  checkRow, checkColumn = [], []
+  for i in range(5):
+    checkRow.append(str(iTablero)+'-'+str(iRow)+'-'+str(i))
+    checkColumn.append(str(iTablero)+'-'+str(i)+'-'+str(index))
+  if all(x in marcados for x in checkRow):
+    return True
+  if all(x in marcados for x in checkColumn):
+    return True
+  return False
+
+def lastWinner(tab, numero):
+  suma = 0
+  for marcador in marcados:
+    splitted = [int(i) for i in marcador.split('-')]
+    tableros[splitted[0]][splitted[1]][splitted[2]] = 0
+  
+  for row in tableros[tab]:
+    suma+=sum(row)
+
+  print('Res=> ' + str(numero*suma))
+  quit()
+
+def letsPlay():
+  i = 0
+  for numero in numeros:
+    iTab=0
+    for tablero in tableros:
+      iRow = 0
+      for row in tablero:
+        if numero in row:
+          marcados.append(str(iTab)+ '-' + str(iRow) + '-' + str(row.index(numero)))
+          if tableroCheck(iTab, iRow, row.index(numero)):
+            if len(tableros) == len(tabsGanadores)+1:
+              if iTab not in tabsGanadores:
+                lastWinner(iTab, numero)
+            else:
+              if iTab not in tabsGanadores:
+                tabsGanadores.append(iTab)
+        iRow+=1
+      iTab+=1
+    i+=1
+
+letsPlay()
