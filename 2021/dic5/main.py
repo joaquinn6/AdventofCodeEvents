6c977fb6fbd962cf62e80a98f513f79a3ddbb576
Joaquin <joaquin.nunez@hotaka.io>
2021-12-05 10:40:43 -0600

    5.1.1

--- 2021/dic5/main.py
+++ 2021/dic5/main.py
@@ -1,36 +1,39 @@
 import lineas
 lineas, puntos = lineas.lineas(), []
 
+
 def countRepetidos():
-  repetidos, unicos = [], []
-  for punto in puntos:
-    if punto not in unicos:
-        unicos.append(punto)
-    elif punto not in repetidos:
-      repetidos.append(punto)
-  return len(repetidos)
+    repetidos, unicos = [], []
+    for punto in puntos:
+        if punto not in unicos:
+            unicos.append(punto)
+        elif punto not in repetidos:
+            repetidos.append(punto)
+    return len(repetidos)
+
+
+def menor_mayor(num1, num2):
+    mayor, menor = 0, 0
+    if num1 < num2:
+        mayor, menor = num2, num1
+    else:
+        mayor, menor = num1, num2
+    return range(menor, mayor+1)
 
 
 def makePoints(linea):
-  mayor, menor=0,0
-  if linea[0] == linea[2]:
-    if linea[1] < linea[3]:
-      mayor, menor = linea[3], linea[1]
-    else:
-      mayor, menor = linea[1], linea[3]
-    for i in range(menor, mayor+1):
-      puntos.append(str(linea[0]) + ',' + str(i))
-  if linea[1]== linea[3]:
-    if linea[0] < linea[2]:
-      mayor, menor = linea[2], linea[0]
-    else:
-      mayor, menor = linea[0], linea[2]
-    for i in range(menor, mayor+1):
-      puntos.append(str(i) +','+ str(linea[3]))
+    if linea[0] == linea[2]:
+        for i in menor_mayor(linea[1], linea[3]):
+            puntos.append(str(linea[0]) + ',' + str(i))
+    elif linea[1] == linea[3]:
+        for i in menor_mayor(linea[0], linea[2]):
+            puntos.append(str(i) + ',' + str(linea[3]))
+
 
 def main():
-  for linea in lineas:
-    makePoints(linea)
-  print("Res=> " + str(countRepetidos())) 
+    for linea in lineas:
+        makePoints(linea)
+    print("Res=> " + str(countRepetidos()))
+
 
 main()
