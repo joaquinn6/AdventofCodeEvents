d44ba09892cb8129d305b232bbf8aa6025cf06b8
Joaquin <joaquin.nunez@hotaka.io>
2021-12-07 10:26:40 -0600

    mejor

--- 2021/dic7/main.py
+++ 2021/dic7/main.py
@@ -1,28 +1,15 @@
 import posiciones
 inicial = posiciones.posiciones()
-
-def mayor_menos_menor(num1, num2):
-    mayor, menor = 0, 0
-    if num1 < num2:
-        mayor, menor = num2, num1
-    else:
-        mayor, menor = num1, num2
-    return mayor-menor
-
 def calculando(actual):
   gas = 0
   for num in range(len(inicial)):
-    gas += mayor_menos_menor(inicial[num], actual)
+    gas += abs(inicial[num]-actual)
   return gas
-
 def moviendo():
-  best, best_position =10000000000000, inicial[0]
+  best=1e9
   inicial.sort()
   for num in range(inicial[len(inicial)-1]+1):
     gasolina= calculando(num)
-    if gasolina < best:
-      best_position= num
-      best= gasolina
-  print("Result => Posición: ", best_position, " Gasolina: ", best)
-
+    best= gasolina if gasolina < best else best
+  print("Result => Gasolina: ", best)
 moviendo()
--- 2021/dic7/main2.py
+++ 2021/dic7/main2.py
@@ -1,30 +1,18 @@
 import posiciones
-inicial = posiciones.posiciones()
-
-def mayor_menos_menor(num1, num2):
-    mayor, menor = 0, 0
-    if num1 < num2:
-        mayor, menor = num2, num1
-    else:
-        mayor, menor = num1, num2
-    return mayor-menor
+inicial = posiciones.prueba()
 
 def calculando(actual):
   gasTotal = 0
   for num in range(len(inicial)):
-    gas = mayor_menos_menor(inicial[num], actual)
-    for num in range(gas+1):
-      gasTotal+=num
+    gas = abs(inicial[num]- actual)
+    gasTotal+=sum(range(gas+1))
   return gasTotal
-
 def moviendo():
-  best, best_position =10000000000000, inicial[0]
+  best=1e9
   inicial.sort()
   for num in range(inicial[len(inicial)-1]+1):
     gasolina= calculando(num)
-    if gasolina < best:
-      best_position= num
-      best= gasolina
-  print("Result => Posición: ", best_position, " Gasolina: ", best)
+    best= gasolina if gasolina < best else best
+  print("Result => Gasolina: ", best)
 
 moviendo()
