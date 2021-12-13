a3f0da27a524e52c649ba8405f03d83851c3eb8f
Joaquin <joaquin.nunez@hotaka.io>
2021-12-13 09:54:58 -0600

    13.2

---
+++ 2021/dic13/main2.py
@@ -0,0 +1,42 @@
+import instructions
+folds, dots = instructions.folds(), instructions.dots()
+
+def eliminarRepetidos():
+    noRepetidos = []
+    for dot in dots:
+        if dot not in noRepetidos:
+            noRepetidos.append(dot)
+    return noRepetidos
+
+def maximos(puntos):
+  y, x=0,0
+  for punto in puntos:
+    y = punto[0] if punto[0]>y else y
+    x = punto[1] if punto[1]>x else x
+  return y, x
+  
+def draw(puntos):
+  puntoYMax, puntoXMax = maximos(puntos)
+  outputStr = ''
+  for x in range(puntoXMax+1):
+    for y in range(puntoYMax+1):
+      if [y,x] in puntos:
+        outputStr +='#'
+      else:
+        outputStr +='.'
+    outputStr += '\n'
+  print (outputStr)
+
+def fold():
+  for fold in folds:
+    if fold[0] == 'y':
+      for dot in dots:
+        if dot[1] > fold[1]:
+          dot[1] -= (dot[1]- fold[1])*2
+    if fold[0] == 'x':
+      for dot in dots:
+        if dot[0] > fold[1]:
+          dot[0] -= (dot[0]-fold[1])*2
+  draw(eliminarRepetidos())
+  
+fold()
