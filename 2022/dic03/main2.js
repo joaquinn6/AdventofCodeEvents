cebdc46119aea11040f5c233e2d0a231ee1e33b0
joaquin_n6 <joaquinne87@gmail.com>
2022-12-03 19:09:56 -0600

    second

---
+++ 2022/dic03/main2.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+var input=fs.readFileSync(process.cwd()+'/2022/dic03/input.txt').toString().split("\n");
+
+const values = {
+  a: 1, b: 2, c:3 , d:4, e:5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17, r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26,
+  A: 27, B: 28, C:29 , D:30, E:31, F: 32, G: 33, H: 34, I: 35, J: 36, K: 37, L: 38, M: 39, N: 40, O: 41, P: 42, Q: 43, R: 44, S: 45, T: 46, U: 47, V: 48, W: 49, X: 50, Y: 51, Z: 52,
+}
+
+function extractLetter(articulos) {
+  const arr1 = articulos[0].split('')
+  const equal = arr1.filter(letter => articulos[1].includes(letter) && articulos[2].includes(letter))
+  return equal[0]
+}
+const letters = []
+
+for (let i = 0; i<= input.length - 1; i+=3){
+  const articulos = [input[i], input[i+1], input[i+2]]
+  letters.push(extractLetter(articulos));
+}
+console.log(letters);
+
+let sum = 0;
+
+letters.forEach((letter) => {
+  sum += values[letter];
+})
+
+console.log(sum)
