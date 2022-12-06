929b00d274fd0978bab2ce8fdd2d75ae9011ad0e
joaquin_n6 <joaquinne87@gmail.com>
2022-12-06 06:56:10 -0600

    part two 6

--- 2022/dic06/main2.js
+++ 2022/dic06/main2.js
@@ -11,11 +11,11 @@ function includesMoreThanOne(str, array){
   return sum > 1
 }
 
-for (let i = 0; i<=input.length-5; i+=1){
-  var array = input.slice(i, i+4).split('')
+for (let i = 0; i<=input.length-15; i+=1){
+  var array =  input.slice(i, i+14).split('')
   var newArray = array.filter((letter) => includesMoreThanOne(letter, array))
   if (newArray.length == 0){
-    console.log(i+4)
+    console.log(i+14)
     break;
   }
 }
