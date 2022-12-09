aacca4e6a2ffdda72192f7ce1249f35c26701b3f
joaquin_n6 <joaquinne87@gmail.com>
2022-12-08 21:37:27 -0600

    part two 7

--- 2022/dic07/main2.py
+++ 2022/dic07/main2.py
@@ -6,6 +6,7 @@ with open("2022/dic07/input.txt","r") as archivo:
 directories = dict()
 directories_list = list()
 cursor = []
+total_size = []
 
 def comando(line):
   if line[1] == 'cd':
@@ -13,28 +14,36 @@ def comando(line):
       cursor.pop()
     else:
       cursor.append(line[2])
-      checkDir()
+      check_dir()
 
-def checkDir():
+def check_dir():
   directories['/'.join(cursor)] = 0
   directories_list.append('/'.join(cursor))
 
-def fileCount(line):
+def file_count(line):
   size = int(line[0])
+  total_size.append(size)
   cursor_copy = list(cursor)
   for _ in cursor:
     directories['/'.join(cursor_copy)] += size
     cursor_copy.pop()
 
+
 for line in lines:
   if line[0] == "$":
     comando(line)
   elif line[0] != 'dir':
-    fileCount(line)
+    file_count(line)
 
 sizes = list()
 for directory in directories_list:
-  if directories[directory] <= 100000:
-    sizes.append(directories[directory])
+  sizes.append(directories[directory])
 
-print (sum(sizes))
+sizes.sort()
+space_free = 70000000 - sum(total_size)
+space_need = 30000000 - space_free
+
+for size in sizes:
+  if size > space_need:
+    print (size)
+    break
